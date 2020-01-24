203365a06c8a3cd599c7aaa4cb45b20ed5e469cc
Michael Williams <mwilliams2964@gmail.com>
2020-01-23 20:58:22 -0700

    added another comment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 // this is from Michael
+// and this is Michael's second comment for push #2
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
