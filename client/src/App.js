f222b8fbb406b89ff1514a5d17a0f286e034f85b
Sara Gryka <sarakgryka@hotmail.com>
2020-01-23 19:35:21 -0700

    auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+
 import Trips from "./pages/trips";
-import Nav from "./components/Nav";
+
+// src/App.js
+
+import React from "react";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <Nav />
-     
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
     </div>
   );
 }
