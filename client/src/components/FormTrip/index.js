77c49a7a52a285e57a0f90d6c9074c83626b0ab3
Michael Williams <mwilliams2964@gmail.com>
2020-02-11 15:27:56 -0700

    added redirect upon click of saved trip

--- client/src/components/FormTrip/index.js
+++ client/src/components/FormTrip/index.js
@@ -1,42 +1,23 @@
+import '../style.css';
+import React, { Fragment } from 'react';
 
-import "../style.css";
-import React, { Fragment } from "react";
-import { useAuth0 } from "../../react-auth0-spa";
 function FormTrip(props) {
-    const { user } = useAuth0();
-    return (
-        <div className="container tripFormStyle">
-            {/* <Fragment>
+  return (
+    <div className="container tripFormStyle">
+      {/* <Fragment>
                <h3> Welcome, {user.name} ! </h3>
                 </Fragment> */}
 
-            <form className="col-sm-12">
-                <h3>Search Here</h3>
-                <div className="form-row">
-                    <div className="col">
-                        <input
-                            onChange={props.handleInput}
-                            type="text"
-                            value={props.startLocation}
-                            class="form-control search-loc location"
-                            placeholder="Starting point..."
-                            id="start"
-                            name="start"
-                        />
-
-                    </div>
-                    <div class="col">
-                        <input
-                            onChange={props.handleInput}
-                            type="text"
-                            value={props.endLocation}
-                            class="form-control search-loc location"
-                            placeholder="End point..."
-                            id="end"
-                            name="end"
-                        />
-                    </div>
-                    {/* <div
+      <form className="col-sm-12">
+        <h3>Search Here</h3>
+        <div className="form-row">
+          <div className="col">
+            <input onChange={props.handleInput} type="text" value={props.startLocation} class="form-control search-loc location" placeholder="Starting point..." id="start" name="start" />
+          </div>
+          <div class="col">
+            <input onChange={props.handleInput} type="text" value={props.endLocation} class="form-control search-loc location" placeholder="End point..." id="end" name="end" />
+          </div>
+          {/* <div
                    onChange={props.handleInput}
                    value={user.name}
                    id="user"
@@ -44,20 +25,16 @@ function FormTrip(props) {
                    >
 
                    </div> */}
-                    <div className="col">
-                        <button onClick={() => props.handleFormSubmit(user.nickname)}
-                            type="button"
-                            class="btn btn-primary hvr-sweep-to-right"
-                            id="searchButton">Search
-                            <i class="fas fa-search">
-                            </i>
-                        </button>
-                    </div>
-                </div>
-            </form >
+          <div className="col">
+            <button onClick={() => props.handleFormSubmit()} type="button" class="btn btn-primary hvr-sweep-to-right" id="searchButton">
+              Search
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 export default FormTrip;
-
--- client/src/components/tripSearchResults/index.js
+++ client/src/components/tripSearchResults/index.js
@@ -1,83 +1,82 @@
-import React from "react";
-import "../style.css";
+import React from 'react';
+import '../style.css';
+import { useAuth0 } from '../../react-auth0-spa';
 
 function TripSearchResults(props) {
+    const { user } = useAuth0();
     return (
-
         <div>
+            {props.steps ?
 
-            {
-                props.steps.length ?
-                    <div className="container listSearchResults">
+                <div className="container listSearchResults">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <button onClick={() => props.saveTrip(user.nickname)} type="button" class="btn btn-primary hvr-sweep-to-right" id="searchButton">
+                                Save
+            <i class="fas fa-floppy"></i>
+                            </button>
+                        </div>
+                    </div>
 
-                        <div className="row">
-                            <div className="col-sm-12">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <h3>Directions:</h3>
 
-                                <h3>Directions:</h3>
-
-                                <div className="directionResults">
-                                    {props.steps.map(step => (
-                                        <ul key={step.html_instructions}>
-                                            <li>{step.html_instructions.replace(/<\/?[^>]+(>|$)/g, "")}</li>
-                                        </ul>
-                                    ))}
-                                </div>
+                            <div className="directionResults">
+                                {props.steps.map(step => (
+                                    <ul key={step.html_instructions}>
+                                        <li>{step.html_instructions.replace(/<\/?[^>]+(>|$)/g, '')}</li>
+                                    </ul>
+                                ))}
                             </div>
+                        </div>
+                    </div>
 
+                    <br></br>
+
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <h3>Places to Stay:</h3>
+
+                            <div className="stayResults">
+                                {props.lodging.map(hotel => (
+                                    <ul key={hotel}>
+                                        <li>{hotel}</li>
+                                    </ul>
+                                ))}
+                            </div>
                         </div>
 
                         <br></br>
 
+                        <div className="col-sm-4">
+                            <h3>Places to Eat:</h3>
 
-                        <div className="row">
-                            <div className="col-sm-4">
+                            <div className="eatResults">
+                                {props.restaurants.map(restaurant => (
+                                    <ul key={restaurant}>
+                                        <li>{restaurant}</li>
+                                    </ul>
+                                ))}
+                            </div>
+                        </div>
 
-                                <h3>Places to Stay:</h3>
+                        <div className="col-sm-4">
+                            <h3>Things to see at your destination:</h3>
 
-                                <div className="stayResults">
-                                    {props.lodging.map(hotel => (
-                                        <ul key={hotel}>
-                                            <li>{hotel}</li>
-                                        </ul>
-                                    ))}
-                                </div>
+                            <div className="landmarkResults">
+                                {props.placesOfInterest.map(place => (
+                                    <ul key={place}>
+                                        <li>{place}</li>
+                                    </ul>
+                                ))}
                             </div>
 
-                            <br></br>
-
-                            <div className="col-sm-4">
-
-                                <h3>Places to Eat:</h3>
-
-                                <div className="eatResults">
-                                    {props.restaurants.map(restaurant => (
-                                        <ul key={restaurant}>
-                                            <li>{restaurant}</li>
-                                        </ul>
-                                    ))}
-                                </div>
-
-
-                            </div>
-
-
-                            <div className="col-sm-4">
-
-                                <h3>Things to see at your destination:</h3>
-
-                                <div className="landmarkResults">
-                                    {props.placesOfInterest.map(place => (
-                                        <ul key={place}>
-                                            <li>{place}</li>
-                                        </ul>
-                                    ))}
-                                </div>
-                            </div>
 
                         </div>
                     </div>
-                    : null
-            }
+                </div>
+                : null}
         </div>
     );
 }
