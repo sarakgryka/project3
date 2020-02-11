70821420df2d27c7b6ce4284a8cfdfc5bebf2577
Michael Williams <mwilliams2964@gmail.com>
2020-02-11 14:47:59 -0700

    got rid of placeholder titles under Map

--- client/src/components/tripSearchResults/index.js
+++ client/src/components/tripSearchResults/index.js
@@ -4,87 +4,80 @@ import "../style.css";
 function TripSearchResults(props) {
     return (
 
+        <div>
 
-        <div className="container listSearchResults">
+            {
+                props.steps.length ?
+                    <div className="container listSearchResults">
 
-            <div className="row">
-                <div className="col-sm-12">
+                        <div className="row">
+                            <div className="col-sm-12">
 
-                    <h3>Directions:</h3>
+                                <h3>Directions:</h3>
 
-                    <div className="directionResults">
-                        {props.steps.map(step => (
-                            <ul key={step.html_instructions}>
-                                <li>{step.html_instructions.replace(/<\/?[^>]+(>|$)/g, "")}</li>
-                            </ul>
-                        ))}
+                                <div className="directionResults">
+                                    {props.steps.map(step => (
+                                        <ul key={step.html_instructions}>
+                                            <li>{step.html_instructions.replace(/<\/?[^>]+(>|$)/g, "")}</li>
+                                        </ul>
+                                    ))}
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <br></br>
+
+
+                        <div className="row">
+                            <div className="col-sm-4">
+
+                                <h3>Places to Stay:</h3>
+
+                                <div className="stayResults">
+                                    {props.lodging.map(hotel => (
+                                        <ul key={hotel}>
+                                            <li>{hotel}</li>
+                                        </ul>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <br></br>
+
+                            <div className="col-sm-4">
+
+                                <h3>Places to Eat:</h3>
+
+                                <div className="eatResults">
+                                    {props.restaurants.map(restaurant => (
+                                        <ul key={restaurant}>
+                                            <li>{restaurant}</li>
+                                        </ul>
+                                    ))}
+                                </div>
+
+
+                            </div>
+
+
+                            <div className="col-sm-4">
+
+                                <h3>Things to see at your destination:</h3>
+
+                                <div className="landmarkResults">
+                                    {props.placesOfInterest.map(place => (
+                                        <ul key={place}>
+                                            <li>{place}</li>
+                                        </ul>
+                                    ))}
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-
-            </div>
-
-            <br></br>
-
-
-            <div className="row">
-                <div className="col-sm-4">
-
-                    <h3>Places to Stay:</h3>
-
-                    <div className="stayResults">
-                    {props.lodging.map(hotel => (
-                            <ul key={hotel}>
-                                <li>{hotel}</li>
-                            </ul>
-                        ))}
-                    </div>
-                    </div>
-
-                    <br></br>
-
-                    <div className="col-sm-4">
-
-                    <h3>Places to Eat:</h3>
-
-                    <div className="eatResults">
-                    {props.restaurants.map(restaurant => (
-                            <ul key={restaurant}>
-                                <li>{restaurant}</li>
-                            </ul>
-                        ))}
-                    </div>
-
-
-                </div>
-
-
-                <div className="col-sm-4">
-
-                    <h3>Things to see at your destination:</h3>
-
-                    <div className="landmarkResults">
-                    {props.placesOfInterest.map(place => (
-                            <ul key={place}>
-                                <li>{place}</li>
-                            </ul>
-                        ))}
-                    </div>
-
-                    {/* <h3>Things to see along the way:</h3>
-
-                    <div className="landmarkResults">
-                        <ul>
-                            <li>Landmark one</li>
-                            <li>Landmark two</li>
-                            <li>Landmark three</li>
-                            <li>Landmark four</li>
-                        </ul>
-                    </div> */}
-
-
-                </div>
-
-            </div>
+                    : null
+            }
         </div>
     );
 }
